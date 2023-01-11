987f82978d7f81ab94762733c3c63c745fb4dea4
raykumar123 <eco20007@tezu.ac.in>
2023-01-11 22:23:32 +0530

    task4

---
+++ task3/task4/main.js
@@ -0,0 +1,8 @@
+var items = document.getElementsByClassName('list-group-item');
+//console.log(items);
+//items[1].textContent='hello';
+items[0].style.fontWeight ='bold';
+items[1].style.fontWeight ='bold';
+items[2].style.fontWeight ='bold';
+items[3].style.fontWeight ='bold';
+items[2].style.backgroundColor='green';
