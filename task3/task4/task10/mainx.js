505c0f28c4b46a69721aed461d036c0dd0872906
raykumar123 <eco20007@tezu.ac.in>
2023-01-17 22:12:27 +0530

    ques12

--- task3/task4/task10/mainx.js
+++ task3/task4/task10/mainx.js
@@ -1,38 +1,17 @@
-const myForm = document.querySelector('#my-form');
-const nameInput = document.querySelector('#name');
-const emailInput = document.querySelector('#email');
-const msg = document.querySelector('.msg');
-const userList = document.querySelector('#users');
 
-// Listen for form submit
-myForm.addEventListener('submit', onSubmit);
+function saveToLocalStorage(event) {
+    event.preventDefault();
 
-function onSubmit(e) {
-  e.preventDefault();
-  
-  if(nameInput.value === '' || emailInput.value === '') {
-    // alert('Please enter all fields');
-    msg.classList.add('error');
-    msg.innerHTML = 'Please enter all fields';
-
-    // Remove error after 3 seconds
-    setTimeout(() => msg.remove(), 3000);
-  } else {
-    // Create new list item with user
-    const li = document.createElement('li');
-
-    // Add text node with input values
-    li.appendChild(document.createTextNode(`${nameInput.value}: ${emailInput.value}`));
-
-    // Add HTML
-    // li.innerHTML = `<strong>${nameInput.value}</strong>e: ${emailInput.value}`;
-
-    // Append to ul
-    userList.appendChild(li);
-
-    // Clear fields
-   nameInput.value = '';
-    emailInput.value = '';
-    localStorage.setItem('name','name')
-  }
+        const name = event.target.username.value;
+        const email = event.target.emailId.value;
+        var myobj = {
+            name,
+            email
+        }
+        localStorage.setItem('userDetails', JSON.stringify(myobj));
+    }
+function showUserOnScreen(obj){
+    const parentElem =document.getElementById('listOfitems')
+    const childElem =document.createElement('li')
+   parentElem.innerHTML=parentElem.innerHTML+ `<li>${myobj.name} - ${myobj.email}</li>`
 }
