ec9571347c64cd9ac5af08c9a3394b7bc9f58aa0
raykumar123 <eco20007@tezu.ac.in>
2023-01-19 08:05:49 +0530

    ques14

--- task3/task4/task10/mainx.js
+++ task3/task4/task10/mainx.js
@@ -1,17 +1,39 @@
-
 function saveToLocalStorage(event) {
     event.preventDefault();
 
         const name = event.target.username.value;
         const email = event.target.emailId.value;
-        var myobj = {
+        var obj = {
             name,
             email
         }
-        localStorage.setItem('userDetails', JSON.stringify(myobj));
+        localStorage.setItem('userDetails', JSON.stringify(obj));
+        showUserOnScreen(obj)
     }
 function showUserOnScreen(obj){
     const parentElem =document.getElementById('listOfitems')
     const childElem =document.createElement('li')
-   parentElem.innerHTML=parentElem.innerHTML+ `<li>${myobj.name} - ${myobj.email}</li>`
+   //parentElem.innerHTML=parentElem.innerHTML+ `<li>${obj.name} - ${obj.email}</li>`
+   childElem.textContent=obj.name + ' - ' + obj.email + ' -  '
+   const deleteButton =document.createElement('input')
+   deleteButton.type="button"
+   deleteButton.value='Delete'
+   deleteButton.onclick =() => {
+    localStorage.removeItem(obj.email)
+    parentElem.removeChild(childElem)
+   }
+
+  const editButton =document.createElement('input')
+    editButton.type="button"
+    editButton.value='Edit'
+   editButton.onclick =() => {
+     localStorage.removeItem(obj.email)
+    parentElem.removeChild(childElem)
+    document.getElementById('usernameInputTag').value=obj.name
+    document.getElementById('emailIdInputTag').value=obj.email
+   }
+   
+   childElem.appendChild(deleteButton)
+   childElem.appendChild(editButton)
+   parentElem.appendChild(childElem)
 }
