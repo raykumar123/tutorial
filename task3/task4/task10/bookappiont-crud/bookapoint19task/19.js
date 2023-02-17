f9c6eca71dc842db2e3ef736b0911d632c2993a8
raykumar123 <eco20007@tezu.ac.in>
2023-02-17 13:02:18 +0530

    DELAPOINT-19

---
+++ task3/task4/task10/bookappiont-crud/bookapoint19task/19.js
@@ -0,0 +1,87 @@
+
+////DELETE USER DETAILS USING NETWORK CALL VIA AXIOS
+
+function saveToLocalStorage(event) {
+    event.preventDefault();
+
+       const  expense = event.target.expense.value;
+        const emailId = event.target.emailId.value;
+        const  description = event.target.description.value;
+        var obj = {
+            expense,
+            emailId,
+            description
+        }
+        axios.post("https://crudcrud.com/api/33661a5b08014da4a67d370229c3242a/expensetracker",obj)
+        .then((respone) => {
+            showUserOnScreen(respone.data)
+            //console.log(Response)
+        }).catch((err) => {
+            console.log(err)
+        })
+        // localStorage.setItem(obj.expense, JSON.stringify(obj));
+        // localStorage.setItem(obj.email, JSON.stringify(obj));
+        // localStorage.setItem(obj.target, JSON.stringify(obj));
+        //showUserOnScreen(obj)
+    }
+    window.addEventListener("DOMContentLoaded", () =>{
+        axios.get("https://crudcrud.com/api/33661a5b08014da4a67d370229c3242a/expensetracker")
+        .then((respone) => {
+            console.log(respone)
+            for(var i=0; i<respone.data.length;i++){
+                showUserOnScreen(respone.data[i])
+            }
+        })
+        .catch((error) =>{
+            console.log(error)
+        })
+    })
+
+    // window.addEventListener("DOMContentLoaded",() =>{
+    //     const localStorageObj = localStorage;
+    //     const localStoragekeys = Object.keys(localStorageObj)
+
+    //     for(var i=0; i<localStoragekeys.length ;i++){
+    //         const key =localStoragekeys[i]
+    //         const userDetailsString = localStorageObj[key];
+    //         const userDetailsObj =JSON.parse(userDetailsString);
+    //         showUserOnScreen(userDetailsObj);
+    //     }
+    // })
+function showUserOnScreen(obj){
+    const parentElem =document.getElementById('listOfitems')
+    const childElem =document.createElement('li')
+   //parentElem.innerHTML=parentElem.innerHTML+ `<li>${obj.expense} - ${obj.email}</li>-${obj.description}`
+   childElem.textContent=obj.expense + ' - ' + obj._id + ' -  ' +obj.description + ' - '
+   const ExpDelButton =document.createElement('input')
+   ExpDelButton.type="button"
+   ExpDelButton.value='Delete Expense'
+   ExpDelButton.onclick =() => {
+ 
+        axios.delete(`https://crudcrud.com/api/33661a5b08014da4a67d370229c3242a/expensetracker/${obj._id}`)
+        .then((respone) => {
+            removeUsersFromScreen(obj._id)
+        })
+        .catch((err) => {
+            console.log(err)
+        })
+    
+    //localStorage.removeItem(obj._id)
+    parentElem.removeChild(childElem)
+   }
+
+  const editButton =document.createElement('input')
+    editButton.type="button"
+    editButton.value='Edit Expense'
+   editButton.onclick =() => {
+     localStorage.removeItem(obj._id)
+    parentElem.removeChild(childElem)
+    document.getElementById('priceInputTag').value = obj.expense
+    document.getElementById('emailInputTag').value = obj._id
+    document.getElementById('itemsInputTag').value = obj.description
+   }
+   
+   childElem.appendChild(ExpDelButton)
+   childElem.appendChild(editButton)
+   parentElem.appendChild(childElem)
+}
